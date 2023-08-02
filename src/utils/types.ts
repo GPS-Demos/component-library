1d8b9195a0782542573f403fd2241570141eb118
Alan Colver <acolver@google.com>
2023-08-02 16:45:28 -0600

    Refine the IFormVariable type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -12,7 +12,7 @@ const FIELD_TYPE = [
   "dob",
 ] as const
 
-const fieldType = z.enum(FIELD_TYPE)
+export const fieldType = z.enum(FIELD_TYPE)
 
 // export const FormVariable = z.object({
 //   name: z.string(),
@@ -48,23 +48,22 @@ const fieldType = z.enum(FIELD_TYPE)
 
 export const FormVariable = z.object({
   name: z.string(),
-  questionId: z.number(),
-  question: z.string(),
+  display: z.string(),
   type: fieldType,
-  description: z.string(),
-  default: z.any().optional(),
-  required: z.boolean(),
+  description: z.string().optional(),
+  default: z.unknown().optional(),
+  required: z.boolean().default(true),
   validations: z.string().optional(),
-  group: z.number(),
-  order: z.number(),
+  group: z.string().optional(),
+  order: z.number().default(999),
   suborder: z.number().optional(),
   primaryId: z.number().optional(),
   choosenOption: z.number().optional(),
   options: z
     .array(
       z.object({
-        oId: z.number(),
-        value: z.string(),
+        display: z.string(),
+        value: z.unknown(),
       }),
     )
     .optional(),
